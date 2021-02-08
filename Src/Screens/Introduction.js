2ee99d3b4b9785472603b7fae943609f2efb6817
Jahanzaib Ali <jhanzeb.ali99@gmail.com>
2021-02-09 01:12:57 +0500

    FacebookSdk , TwitterSdk,FirebaseInitiliazed

---
+++ Src/Screens/Introduction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const Introduction = (props) => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Introduction</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Introduction;
---
+++ Src/Screens/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const Signup = (props) => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Sign up</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Signup;
