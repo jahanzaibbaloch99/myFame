e39e610a11d057f62985c61cdaa305dc223ad4c0
Jahanzaib Ali <jhanzeb.ali99@gmail.com>
2021-02-13 01:35:41 +0500

    fixes

--- Src/Utils/Firebase/Auth.js
+++ Src/Utils/Firebase/Auth.js
@@ -1,4 +1,5 @@
 import Auth from '@react-native-firebase/auth';
+import fireStore from "@react-native-firebase/firestore"
 export const SinginCreater = async (email, password) => {
   try {
     const Res = await Auth()
@@ -19,6 +20,9 @@ export const SignupCreater = async (email, password) => {
       .then((ele) => {
         return JSON.parse(ele.user)
     });
+    if (Res.user) {
+      const await 
+    }
     return Res;
   } catch (e) {
     return e;
